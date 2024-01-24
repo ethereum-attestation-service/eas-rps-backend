55061c8628bd206d8cb38fd17250974621f68d07
jsk11235 <kirmayerjacob@gmail.com>
2024-01-23 19:18:49 -0500

    results table

--- index.ts
+++ index.ts
@@ -5,14 +5,21 @@ import bodyParser from "body-parser";
 import {StoreIPFSActionReturn} from "./types";
 import verificationMiddleware from './verifyAttestation'
 import {
-    SchemaEncoder, AttestationShareablePackageObject, ZERO_BYTES, ZERO_BYTES32
+  SchemaEncoder, AttestationShareablePackageObject, ZERO_BYTES, ZERO_BYTES32
 } from "@ethereum-attestation-service/eas-sdk";
 
 import {PrismaClient, Game} from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-import {CHOICE_UNKNOWN, CUSTOM_SCHEMAS, dbFriendlyAttestation, STATUS_UNKNOWN} from "./utils";
+import {
+  addFinishedGameToPlayerResults,
+  CHOICE_UNKNOWN,
+  CUSTOM_SCHEMAS,
+  dbFriendlyAttestation,
+  STATUS_UNKNOWN
+} from "./utils";
+import {ethers} from 'ethers';
 
 const app = express()
 const port = 8080
@@ -24,98 +31,236 @@ app.use(cors())
 
 // note: build in middleware to verify all attestations
 app.post('/newAttestation', verificationMiddleware, async (req, res) => {
-    const attestation: AttestationShareablePackageObject = JSON.parse(req.body.textJson)
+  const attestation: AttestationShareablePackageObject = JSON.parse(req.body.textJson)
 
-    if (attestation.sig.message.schema === CUSTOM_SCHEMAS.CREATE_GAME_CHALLENGE) {
-        const player1 = attestation.signer
-        const player2 = attestation.sig.message.recipient
-        await prisma.game.create({
-            data: {
-                uid: attestation.sig.uid,
-                player1: player1,
-                player2: player2,
-                commit1: ZERO_BYTES32,
-                commit2: ZERO_BYTES32,
-                choice1: CHOICE_UNKNOWN,
-                choice2: CHOICE_UNKNOWN,
-                salt1: ZERO_BYTES32,
-                salt2: ZERO_BYTES32,
-            }
-        })
-    } else if (attestation.sig.message.schema === CUSTOM_SCHEMAS.COMMIT_HASH) {
-        const schemaEncoder = new SchemaEncoder("bytes32 commitHash");
+  if (attestation.sig.message.schema === CUSTOM_SCHEMAS.CREATE_GAME_CHALLENGE) {
+    const player1 = attestation.signer
+    const player2 = attestation.sig.message.recipient
+    await prisma.game.create({
+      data: {
+        uid: attestation.sig.uid,
+        player1: player1,
+        player2: player2,
+        commit1: ZERO_BYTES32,
+        commit2: ZERO_BYTES32,
+        choice1: CHOICE_UNKNOWN,
+        choice2: CHOICE_UNKNOWN,
+        salt1: ZERO_BYTES32,
+        salt2: ZERO_BYTES32,
+      }
+    })
+  } else if (attestation.sig.message.schema === CUSTOM_SCHEMAS.COMMIT_HASH) {
+    const schemaEncoder = new SchemaEncoder("bytes32 commitHash");
 
-        const commitHash = (schemaEncoder.decodeData(attestation.sig.message.data))[0].value.value.toString();
-        const gameID = attestation.sig.message.refUID;
+    const commitHash = (schemaEncoder.decodeData(attestation.sig.message.data))[0].value.value.toString();
+    const gameID = attestation.sig.message.refUID;
 
-        const players = await prisma.game.findUnique({
-            select: {
-                player1: true,
-                player2: true,
-            },
-            where: {
-                uid: gameID
-            }
-        })
-
-        if (attestation.signer === players!.player1) {
-            await prisma.game.update({
-                where: {
-                    uid: gameID,
-                },
-                data: {
-                    commit1: commitHash
-                }
-            })
-        } else if (attestation.signer === players!.player2) {
-            await prisma.game.update({
-                where: {
-                    uid: gameID,
-                },
-                data: {
-                    commit2: commitHash
-                }
-            })
-        }
-    }
-
-    await prisma.attestation.create({
-        data: dbFriendlyAttestation(attestation)
+    const players = await prisma.game.findUnique({
+      select: {
+        player1: true,
+        player2: true,
+      },
+      where: {
+        uid: gameID
+      }
     })
 
-    const result: StoreIPFSActionReturn = {
-        error: null,
-        ipfsHash: null,
-        offchainAttestationId: attestation.sig.uid
+    if (attestation.signer === players!.player1) {
+      await prisma.game.update({
+        where: {
+          uid: gameID,
+        },
+        data: {
+          commit1: commitHash
+        }
+      })
+    } else if (attestation.signer === players!.player2) {
+      await prisma.game.update({
+        where: {
+          uid: gameID,
+        },
+        data: {
+          commit2: commitHash
+        }
+      })
     }
-    res.json(result)
+  } else if (attestation.sig.message.schema === CUSTOM_SCHEMAS.DECLINE_GAME_CHALLENGE) {
+    const gameID = attestation.sig.message.refUID;
+
+    const players = await prisma.game.findUnique({
+      select: {
+        player2: true,
+      },
+      where: {
+        uid: gameID,
+        commit2: ZERO_BYTES32,
+      }
+    })
+
+    if (!players) {
+      return
+    }
+
+    if (attestation.signer === players.player2) {
+      await prisma.game.update({
+        where: {
+          uid: gameID,
+        },
+        data: {
+          declined: true
+        }
+      })
+    }
+  }
+
+  await prisma.attestation.create({
+    data: dbFriendlyAttestation(attestation)
+  })
+
+  const result: StoreIPFSActionReturn = {
+    error: null,
+    ipfsHash: null,
+    offchainAttestationId: attestation.sig.uid
+  }
+  res.json(result)
 })
 
 app.post('/gameStatus', async (req, res) => {
-    const {uid} = req.body
+  const {uid} = req.body
 
-    const game = await prisma.game.findUnique({
-        where: {
-            uid: uid
-        },
-    })
+  const game = await prisma.game.findUnique({
+    where: {
+      uid: uid
+    },
+  })
 
-    res.json(game)
+  res.json(game)
 })
 
 app.post('/incomingChallenges', async (req, res) => {
-    const {toAddress} = req.body
+  const {toAddress} = req.body
 
-    const challenges = await prisma.game.findMany({
-        where: {
-            player2: toAddress,
-            commit2: ZERO_BYTES32
+  const challenges = await prisma.game.findMany({
+    where: {
+      player2: toAddress,
+      commit2: ZERO_BYTES32,
+      declined: false,
+    },
+  })
+
+  res.json(challenges)
+})
+
+app.post('/gamesPendingReveal', async (req, res) => {
+  const {address} = req.body
+
+  const games = await prisma.game.findMany({
+    select: {
+      uid: true,
+    },
+    where: {
+      commit1: {
+        not: ZERO_BYTES32
+      },
+      commit2: {
+        not: ZERO_BYTES32
+      },
+      OR: [
+        {
+          player1: address,
+          choice1: CHOICE_UNKNOWN
         },
+        {
+          player2: address,
+          choice2: CHOICE_UNKNOWN
+        }
+      ]
+    },
+  });
+
+  res.json(games.map((game) => game.uid))
+
+})
+
+app.post('/revealMany', async (req, res) => {
+  type Reveal = { uid: string, choice: number, salt: string }
+  const {reveals}: { reveals: Reveal[] } = req.body
+
+  for (const reveal of reveals) {
+    const {uid, choice, salt} = reveal
+
+    const game = await prisma.game.findUnique({
+      where: {
+        uid: uid
+      },
     })
 
-    res.json(challenges)
+    if (!game) {
+      continue
+    }
+
+    const hashedChoice = ethers.solidityPackedKeccak256(
+      ["uint256", "bytes32"],
+      [choice, salt]
+    );
+
+    if (hashedChoice === game.commit1) {
+      await prisma.game.update({
+        where: {
+          uid: reveal.uid
+        },
+        data: {
+          choice1: reveal.choice,
+          salt1: reveal.salt,
+        }
+      })
+      if (game.choice2 !== CHOICE_UNKNOWN) {
+        // add logic to disallow duplicate results
+        await addFinishedGameToPlayerResults(game, choice, game.choice2);
+      }
+    } else if (hashedChoice === game.commit2) {
+      await prisma.game.update({
+        where: {
+          uid: reveal.uid
+        },
+        data: {
+          choice2: reveal.choice,
+          salt2: reveal.salt,
+        }
+      })
+      if (game.choice1 !== CHOICE_UNKNOWN) {
+        // add logic to disallow duplicate results
+        await addFinishedGameToPlayerResults(game, game.choice1, choice);
+      }
+    }
+  }
+
+
+  res.json({})
+})
+
+app.post('/myGames', async (req, res) => {
+  const {address} = req.body
+
+  const active = await prisma.game.findMany({
+    where: {
+      OR: [
+        {
+          player1: address,
+          choice2: CHOICE_UNKNOWN,
+        },
+        {
+          player2: address,
+          choice1: CHOICE_UNKNOWN,
+        }
+      ],
+      declined: false,
+    },
+  });
+
+  res.json(active)
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
--- utils.ts
+++ utils.ts
@@ -1,15 +1,18 @@
-import {Attestation} from "@prisma/client";
+import {Attestation, Game, PrismaClient} from "@prisma/client";
 import {AttestationShareablePackageObject} from "@ethereum-attestation-service/eas-sdk";
 
+const prisma = new PrismaClient();
 export const CUSTOM_SCHEMAS = {
-    COMMIT_HASH:
-        "0x2328029cfa84b9ea42f4e0e8fa24fbf66da07ceec0a925dd27370b9617b32d59",
-    CREATE_GAME_CHALLENGE:
-        "0x64b1bac6f531c64a6aa372b1239111fe41a60003dcda62bfa967bc6e4c4d91e0",
+  COMMIT_HASH:
+    "0x2328029cfa84b9ea42f4e0e8fa24fbf66da07ceec0a925dd27370b9617b32d59",
+  CREATE_GAME_CHALLENGE:
+    "0x64b1bac6f531c64a6aa372b1239111fe41a60003dcda62bfa967bc6e4c4d91e0",
+  DECLINE_GAME_CHALLENGE:
+    "0x27e160d185f1d97202897bd3ed697906398b70a8d08b0d22bc2cfffdf561e3e9",
 };
 
 export const RPS_GAME_UID =
-    "0x048de8e6b4bf0769744930cc2641ce05d473f3cd5ce976ba9e6a3256d4b011eb";
+  "0x048de8e6b4bf0769744930cc2641ce05d473f3cd5ce976ba9e6a3256d4b011eb";
 
 export const CHOICE_UNKNOWN = 3;
 
@@ -20,17 +23,49 @@ export const STATUS_PLAYER2_WIN = 2;
 export const STATUS_UNKNOWN = 3;
 export const STATUS_INVALID = 4;
 
+export const RESULT_DRAW = 0;
+export const RESULT_WIN = 1;
+export const RESULT_LOSS = 2;
+
 export function dbFriendlyAttestation(attestation: AttestationShareablePackageObject): Attestation {
-    return {
-        uid: attestation.sig.uid,
-        isOffchain: true,
-        data: attestation.sig.message.data,
-        attester: attestation.signer,
-        recipient: attestation.sig.message.recipient,
-        refUID: attestation.sig.message.refUID,
-        schemaId: attestation.sig.message.schema,
-        signature: JSON.stringify(attestation.sig.signature),
-        gameUID: attestation.sig.message.schema === CUSTOM_SCHEMAS.COMMIT_HASH ? attestation.sig.uid : attestation.sig.message.refUID,
-        onChainTimestamp:0
+  return {
+    uid: attestation.sig.uid,
+    isOffchain: true,
+    data: attestation.sig.message.data,
+    attester: attestation.signer,
+    recipient: attestation.sig.message.recipient,
+    refUID: attestation.sig.message.refUID,
+    schemaId: attestation.sig.message.schema,
+    signature: JSON.stringify(attestation.sig.signature),
+    gameUID: attestation.sig.message.schema === CUSTOM_SCHEMAS.CREATE_GAME_CHALLENGE ? attestation.sig.uid : attestation.sig.message.refUID,
+    onChainTimestamp: 0
+  }
+}
+
+export async function addFinishedGameToPlayerResults(game: Game, choice1: number, choice2: number) {
+  await prisma.result.create({
+    data: {
+      player: {
+        connect: {
+          address: game.player1
+        }
+      },
+      gameUID: game.uid,
+      status: (3 + choice1 - choice2) % 3,
+      timestamp: 0
     }
+  });
+
+  await prisma.result.create({
+    data: {
+      player: {
+        connect: {
+          address: game.player2
+        }
+      },
+      gameUID: game.uid,
+      status: (3 + choice2 - choice1) % 3,
+      timestamp: 0
+    }
+  });
 }
